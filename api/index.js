56d93c3f40a757000d26edb2244bd992ecd918f7
Pranav Teegavarapu <pranavnt@outlook.com>
2020-11-11 14:41:39 -0800

    removing node modules

--- api/index.js
+++ api/index.js
@@ -1,6 +1,25 @@
 require("dotenv").config();
 
 const express = require("express");
+const multer = require("multer");
+const port = 5001;
 const app = express();
 
-app.post("/uploadImg");
+var upload = multer();
+
+app.post("/uploadImg", upload.single("img"), function (req, res) {
+  const encodedImg = req.file.buffer.toString("base64");
+  const options = {
+    method: "POST",
+    uri: "https://api.imgbb.com/1/upload",
+    form: {
+      key: process.env.IMBB_KEY,
+      image: encodedImg,
+    },
+    json: true,
+  };
+
+  res.json({ hello: options });
+});
+
+app.listen(port, () => console.log(`port ${port}`));
