a90075fe2136588f0ce91e3dab6944cb559f7379
Pranav Teegavarapu <pranavnt@outlook.com>
2020-11-11 16:10:31 -0800

    ugghhhhhh

--- api/index.js
+++ api/index.js
@@ -6,20 +6,24 @@ const app = express();
 const multer = require("multer");
 var upload = multer();
 
+const Imgbb = require("imgbbjs");
+
 const port = 5001;
 
 app.post("/uploadImg", upload.single("img"), function (req, res) {
   const encodedImg = req.file.buffer.toString("base64");
 
-  let api = new ApiClient({
-    token: process.env.IMBB_KEY,
-  });
+  console.log(uploadToImbBB(encodedImg));
+  const url = uploadToImbBB(encodedImg);
 
-  const ret = api.upload({ image: encodedImg });
-
-  res.json({
-    ret: ret,
-  });
+  res.json({ url: url });
 });
+
+async function uploadToImbBB(encodedImg) {
+  const imgbb = await new Imgbb({ key: process.env.IMGBB_KEY });
+  var ret = await imgbb.upload(encodedImg, "testing").then((data) => (ret = data)));
+
+  return ret;
+}
 
 app.listen(port, () => console.log(`port ${port}`));
